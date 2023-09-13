33a8523538a352c3c8b1d5f7a382e540f1c1823f
s5033121 <s5033121@griffith.edu.au>
2023-09-14 01:07:25 +1000

    add pandas csv read, task2

--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,19 @@ years = []
 co2 = []
 temp = []
 
+# Read csv
+df = pd.read_csv('climate.csv')
+
+# append values to list
+years = df['Year'].tolist()
+co2 = df['CO2'].tolist()
+temp = df['Temperature'].tolist()
+
+# for testing
+#print("Years:", years)
+#print("CO2:", co2)
+#print("Temperature:", temp)
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
