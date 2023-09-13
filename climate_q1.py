91e0c279a8a91d57e6f42eb8c012edb34bc3a986
s5033121 <s5033121@griffith.edu.au>
2023-09-14 00:48:52 +1000

    added list value from table key values

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,37 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('climate.db')
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Execute a query to select data from the ClimateData table
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+
+# Fetch all rows from the table
+rows = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
+
+# add to lists
+for row in rows:
+    year, co2_value, temp_value = row
+    years.append(year)
+    co2.append(co2_value)
+    temp.append(temp_value)
+
+# Close cursor and database conn
+cursor.close()
+conn.close()
+
+# for testing
+#print("Years:", years)
+#print("CO2:", co2)
+#print("Temperature:", temp)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
